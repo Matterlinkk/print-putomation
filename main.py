a2d604a24d6dbbc189ea8b4b5e8cffb2b289b5a8
Matterlinkk <122081802+Matterlinkk@users.noreply.github.com>
2023-07-10 15:50:38 +0300

    Add imports main.py

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
+from scan import Scan
+from pillow import place_photo
+from printer import print_file
 import os
 import time
 
 
 scanner = Scan()
 
+def main():
+    while True:
 
-while True:
+        value = scanner.scan_new_files()
+        png = scanner.scan_png()
 
-    value = scanner.scan_new_files()
-    png = scanner.scan_png()
+        if value:
+            print(f"New file: {value[0]}")
+            place_photo(os.path.join(scanner.folder_photos, value[0]), f"{value[0].split('.')[0]}.png")
+            scanner.move_file(value[0], folder="photos")
 
-    if value:
-        print(f"New file: {value[0]}")
-        place_photo(os.path.join(scanner.folder_photos, value[0]), f"{value[0].split('.')[0]}.png")
-        scanner.move_file(value[0], folder="photos")
+        if png:
+            print_file(os.path.join(scanner.for_print, png[0]))
+            scanner.move_file(png[0], folder="for_print")
 
-    if png:
-        print_file(os.path.join(scanner.for_print, png[0]))
-        scanner.move_file(png[0], folder="for_print")
+        time.sleep(15)
 
-    time.sleep(15)
+
+if __name__ == "__main__":
+    main()
