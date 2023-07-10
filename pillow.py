dd605f899fdaf4fa272aab77b0b81ab6d3cf5b54
Matterlinkk <122081802+Matterlinkk@users.noreply.github.com>
2023-07-10 14:56:41 +0300

    Update pillow.py

--- pillow.py
+++ pillow.py
@@ -1,15 +1,26 @@
 import PIL
 from PIL import Image
 
+#select the desired option(CENTER OR RIGHT)
+FOR_CENTERED_PICTURES = (529, 297)
+FOR_RIGHT_SIDE_PICTURE = (345, 194)
+
 
 def place_photo(file, file_name):
     try:
         im = Image.open(file)
+        im = im.resize(FOR_RIGHT_SIDE_PICTURE)
+        im = im.convert("L")
         a4im = Image.new("RGBA",
                          (595, 842), #A4 at 72DPI
                          (255, 255, 255, 0))
         image_width, image_height = im.size
-        offset = (a4im.width - image_width, 100)
+        # Two identical options must be selected (CENTER OR RIGHT)
+
+        CENTERED_IMAGE = (a4im.width - image_width) // 2, 110
+        RIGHT_SIDE_IMAGE = ((a4im.width - image_width) // 2 + 90, 103)
+
+        offset = (RIGHT_SIDE_IMAGE)
         a4im.paste(im, offset)
         a4im.save(fr"C:\Users\UA\PycharmProjects\BetterCallGoose\for_print\{file_name}", "PNG", quality=100)
         print(f"File: {file} saved")
