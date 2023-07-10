ae1751075e4b2767de15b4bd6e900c765128fb77
Matterlinkk <122081802+Matterlinkk@users.noreply.github.com>
2023-07-10 14:57:22 +0300

    Update printer.py

--- printer.py
+++ printer.py
@@ -4,85 +4,44 @@ import win32ui
 from PIL import Image, ImageWin
 
 
+def print_file(file_path):
 
-class Printer():
+        try:
+            HORZRES = 8
+            VERTRES = 10
 
-    def __init__(self):
-        self.printer = self.__get_printer_names()[0]
+            PHYSICALWIDTH = 110
+            PHYSICALHEIGHT = 111
 
+            printer_name = win32print.GetDefaultPrinter()
 
-    def print_file(self, file_path):
-        HORZRES = 8
-        VERTRES = 10
-        #
-        # LOGPIXELS = dots per inch
-        #
-        LOGPIXELSX = 88
-        LOGPIXELSY = 90
-        #
-        # PHYSICALWIDTH/HEIGHT = total area
-        #
-        PHYSICALWIDTH = 110
-        PHYSICALHEIGHT = 111
-        #
-        # PHYSICALOFFSETX/Y = left / top margin
-        #
-        PHYSICALOFFSETX = 112
-        PHYSICALOFFSETY = 113
+            hDC = win32ui.CreateDC()
+            hDC.CreatePrinterDC(printer_name)
+            printable_area = hDC.GetDeviceCaps(HORZRES), hDC.GetDeviceCaps(VERTRES)
+            printer_size = hDC.GetDeviceCaps(PHYSICALWIDTH), hDC.GetDeviceCaps(PHYSICALHEIGHT)
 
-        printer_name = win32print.GetDefaultPrinter()
+            bmp = Image.open(file_path)
+            if bmp.size[0] > bmp.size[1]:
+                bmp = bmp.rotate(90)
 
-        #
-        # You can only write a Device-independent bitmap
-        #  directly to a Windows device context; therefore
-        #  we need (for ease) to use the Python Imaging
-        #  Library to manipulate the image.
-        #
-        # Create a device context from a named printer
-        #  and assess the printable size of the paper.
-        #
-        hDC = win32ui.CreateDC()
-        hDC.CreatePrinterDC(printer_name)
-        printable_area = hDC.GetDeviceCaps(HORZRES), hDC.GetDeviceCaps(VERTRES)
-        printer_size = hDC.GetDeviceCaps(PHYSICALWIDTH), hDC.GetDeviceCaps(PHYSICALHEIGHT)
-        printer_margins = hDC.GetDeviceCaps(PHYSICALOFFSETX), hDC.GetDeviceCaps(PHYSICALOFFSETY)
+            ratios = [1.0 * printable_area[0] / bmp.size[0], 1.0 * printable_area[1] / bmp.size[1]]
+            scale = min(ratios)
 
-        #
-        # Open the image, rotate it if it's wider than
-        #  it is high, and work out how much to multiply
-        #  each pixel by to get it as big as possible on
-        #  the page without distorting.
-        #
-        bmp = Image.open(file_path)
-        if bmp.size[0] > bmp.size[1]:
-            bmp = bmp.rotate(90)
+            hDC.StartDoc(file_path)
+            hDC.StartPage()
 
-        ratios = [1.0 * printable_area[0] / bmp.size[0], 1.0 * printable_area[1] / bmp.size[1]]
-        scale = min(ratios)
+            dib = ImageWin.Dib(bmp)
+            scaled_width, scaled_height = [int(scale * i) for i in bmp.size]
+            x1 = int((printer_size[0] - scaled_width) / 2)
+            y1 = int((printer_size[1] - scaled_height) / 2)
+            x2 = x1 + scaled_width
+            y2 = y1 + scaled_height
+            dib.draw(hDC.GetHandleOutput(), (x1, y1, x2, y2))
 
-        #
-        # Start the print job, and draw the bitmap to
-        #  the printer device at the scaled size.
-        #
-        hDC.StartDoc(file_path)
-        hDC.StartPage()
+            hDC.EndPage()
+            hDC.EndDoc()
+            hDC.DeleteDC()
 
-        dib = ImageWin.Dib(bmp)
-        scaled_width, scaled_height = [int(scale * i) for i in bmp.size]
-        x1 = int((printer_size[0] - scaled_width) / 2)
-        y1 = int((printer_size[1] - scaled_height) / 2)
-        x2 = x1 + scaled_width
-        y2 = y1 + scaled_height
-        dib.draw(hDC.GetHandleOutput(), (x1, y1, x2, y2))
-
-        hDC.EndPage()
-        hDC.EndDoc()
-        hDC.DeleteDC()
-
-    def __get_printer_names(self):
-        printer_names = []
-        printers = win32print.EnumPrinters(win32print.PRINTER_ENUM_LOCAL | win32print.PRINTER_ENUM_CONNECTIONS)
-        for printer in printers:
-            printer_name = printer[2]
-            printer_names.append(printer_name)
-        return printer_names
+        except Exception as ex:
+            print(ex)
+            
