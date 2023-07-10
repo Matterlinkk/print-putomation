5de170b29a647368ae5b4c67bd348159c548fe51
Matterlinkk <122081802+Matterlinkk@users.noreply.github.com>
2023-07-10 15:51:28 +0300

    error handler removed

--- printer.py
+++ printer.py
@@ -6,42 +6,37 @@ from PIL import Image, ImageWin
 
 def print_file(file_path):
 
-        try:
-            HORZRES = 8
-            VERTRES = 10
+    HORZRES = 8
+    VERTRES = 10
 
-            PHYSICALWIDTH = 110
-            PHYSICALHEIGHT = 111
+    PHYSICALWIDTH = 110
+    PHYSICALHEIGHT = 111
 
-            printer_name = win32print.GetDefaultPrinter()
+    printer_name = win32print.GetDefaultPrinter()
 
-            hDC = win32ui.CreateDC()
-            hDC.CreatePrinterDC(printer_name)
-            printable_area = hDC.GetDeviceCaps(HORZRES), hDC.GetDeviceCaps(VERTRES)
-            printer_size = hDC.GetDeviceCaps(PHYSICALWIDTH), hDC.GetDeviceCaps(PHYSICALHEIGHT)
+    hDC = win32ui.CreateDC()
+    hDC.CreatePrinterDC(printer_name)
+    printable_area = hDC.GetDeviceCaps(HORZRES), hDC.GetDeviceCaps(VERTRES)
+    printer_size = hDC.GetDeviceCaps(PHYSICALWIDTH), hDC.GetDeviceCaps(PHYSICALHEIGHT)
 
-            bmp = Image.open(file_path)
-            if bmp.size[0] > bmp.size[1]:
-                bmp = bmp.rotate(90)
+    bmp = Image.open(file_path).convert("L")
+    if bmp.size[0] > bmp.size[1]:
+        bmp = bmp.rotate(90)
 
-            ratios = [1.0 * printable_area[0] / bmp.size[0], 1.0 * printable_area[1] / bmp.size[1]]
-            scale = min(ratios)
+    ratios = [1.0 * printable_area[0] / bmp.size[0], 1.0 * printable_area[1] / bmp.size[1]]
+    scale = min(ratios)
 
-            hDC.StartDoc(file_path)
-            hDC.StartPage()
+    hDC.StartDoc(file_path)
+    hDC.StartPage()
 
-            dib = ImageWin.Dib(bmp)
-            scaled_width, scaled_height = [int(scale * i) for i in bmp.size]
-            x1 = int((printer_size[0] - scaled_width) / 2)
-            y1 = int((printer_size[1] - scaled_height) / 2)
-            x2 = x1 + scaled_width
-            y2 = y1 + scaled_height
-            dib.draw(hDC.GetHandleOutput(), (x1, y1, x2, y2))
+    dib = ImageWin.Dib(bmp)
+    scaled_width, scaled_height = [int(scale * i) for i in bmp.size]
+    x1 = int((printer_size[0] - scaled_width) / 2)
+    y1 = int((printer_size[1] - scaled_height) / 2)
+    x2 = x1 + scaled_width
+    y2 = y1 + scaled_height
+    dib.draw(hDC.GetHandleOutput(), (x1, y1, x2, y2))
 
-            hDC.EndPage()
-            hDC.EndDoc()
-            hDC.DeleteDC()
-
-        except Exception as ex:
-            print(ex)
-            
+    hDC.EndPage()
+    hDC.EndDoc()
+    hDC.DeleteDC()
